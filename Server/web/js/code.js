f064af96bff825cec5fc5eb2c65ec3f878226fd1
Shifar Shifz <shifar.shifz@gmail.com>
2015-11-06 16:01:00 +0530

    WEB Version finished

--- Server/web/js/code.js
+++ Server/web/js/code.js
@@ -25,13 +25,17 @@ $(document).ready(function () {
 
     var isWorking = false;
 
-    function showProgressBar(){
-        $("#divProgress").css({'display':'block'});
+    function showProgressBar() {
+        $("#divProgress").css({'display': 'block'});
         $("#divProgress").slideDown(2000);
     }
 
-    function hideProgress(){
+    function hideProgress() {
         $("#divProgress").slideUp(2000);
+    }
+
+    function consoleData(data){
+        $("#console_data").prepend('<p>'+ data + '</p>');
     }
 
     $("#bRankix").click(function () {
@@ -50,29 +54,54 @@ $(document).ready(function () {
 
             postProgress(10, "Contacting Tree Manager...");
 
-            $.post(
-                "/Rankix/Tree",
-                {tree: treeData},
-                function (data) {
+            isWorking = true;
+            $("#bRankix").removeClass("btn-primary").addClass("btn-disabled");
 
-                    isWorking = true;
-                    $("#bRankix").removeClass("btn-primary").addClass("btn-disabled");
+            $.post("/Rankix/Tree", {tree: treeData})
+                .done(function (data) {
+
+                    consoleData(data);
 
                     if (data.error) {
                         postProgress(0, "");
                         hideProgress();
                         $("#bRankix").addClass("btn-primary").removeClass("btn-disabled");
-                        $("div#results").append("<div class='alert alert-danger'><strong>Error:</strong>" + data.data + "</div>");
+                        $("div#results").prepend("<div class='alert alert-danger'><strong>Error:</strong>" + data.data + "</div>");
                         isWorking = false;
+                        consoleData(data.data);
                     } else {
 
                         postProgress(10, "Opening socket ...");
 
                         var movieNameAndId = [];
 
+                        /*{
+                         "ignored_element_count":14,
+                         "total_elements_found":18,
+                         "rankixed_file_count":0,
+                         "movie_file_count":4}*/
+
+                        var totalElementsCount = data.total_elements_found;
+                        var rankixedFileCount = data.rankixed_file_count;
+                        var ignoredFileCount = data.ignored_element_count;
+                        var fineFileCount = data.movie_file_count;
+
+                        consoleData("Total elements count: "+totalElementsCount);
+                        consoleData("Rankixed file count: "+rankixedFileCount);
+                        consoleData("Ignored file count: "+ignoredFileCount);
+                        consoleData("Fine file count: "+fineFileCount);
+
+                        if(fineFileCount==0){
+                            isWorking = false;
+                            $("#bRankix").addClass("btn-primary").removeClass("btn-disabled");
+                            $("div#results").prepend("<p class='text-info'>No fine file found!</p>");
+                            return;
+                        }
+
+
                         $("div#results").html("");
 
-                        webSocket = new WebSocket("ws://shifar-shifz.rhcloud.com:8000/Rankix/RankixSocket");
+                        webSocket = new WebSocket("ws://shifar-shifz.rhcloud.com:8000/Rankix/RankixSocket"  );
 
                         webSocket.onopen = function (evt) {
 
@@ -84,45 +113,54 @@ $(document).ready(function () {
 
 
                         webSocket.onmessage = function (evt) {
+
+
                             var data = JSON.parse(evt.data);
+                            var movieName = movieNameAndId[data.id];
+
                             if (!data.error) {
-
                                 var fontSize = data.data * 5;
-                                var movieName = movieNameAndId[data.id];
-                                $("div#results").append('<p style="font-size:' + fontSize + 'px;">' + movieName + "<small class='text-muted'> has " + data.data + "</small><p>");
-
-                                var scoreCount = $("#results").children().size() / 2;
-                                console.log("ScoreCount:" + scoreCount);
-                                console.log("TotalLength:" + movieNameAndId.length);
-                                var perc = (scoreCount / movieNameAndId.length ) * 100;
-                                console.log("Percentage" + perc);
-                                postProgress(perc, parseInt(perc) + "% - Last rankixed : " + movieName);
-
-                                if (perc == 100) {
-                                    hideProgress();
-                                    webSocket.close();
-                                }
+                                $("div#results").prepend('<p style="font-size:' + fontSize + 'px;">' + movieName + '<small class="text-muted"> has ' + data.data + '</small></p>');
 
                             } else {
-                                $("div#results").append("<p class='text-danger'>" + data.data + "</p>");
+
+                                var myRegexp = /^IMDB rating is null for (.+)$/;
+                                var match = myRegexp.exec(data.data);
+                                movieName = match[1];
+                                $("div#results").prepend('<p class="text-danger"><strong>' + movieName + '</strong> has no rating</p>');
+                            }
+
+                            var scoreCount = $("#results p").length;
+
+                            //console.log("ScoreCount:" + scoreCount);
+                            //console.log("TotalLength:" + movieNameAndId.length);
+                            var perc = (scoreCount / movieNameAndId.length ) * 100;
+                            //console.log("Percentage" + perc);
+                            postProgress(perc, parseInt(perc) + "% - Last rankixed : " + movieName);
+
+                            if (perc == 100) {
+                                hideProgress();
+                                webSocket.close();
                             }
                         };
                         webSocket.onclose = function (evt) {
                             isWorking = false;
                             $("#bRankix").addClass("btn-primary").removeClass("btn-disabled");
-                            $("div#results").append("<p class='text-info'>SOCKET Closed</p>");
+                            $("div#results").prepend("<p class='text-info'>SOCKET Closed</p>");
                         };
                         webSocket.onerror = function (evt) {
                             isWorking = false;
                             $("#bRankix").addClass("btn-primary").removeClass("btn-disabled");
-                            $("div#results").append("<p class='text-danger'>" + evt.data + "</p>");
+                            $("div#results").prepend("<p class='text-danger'>" + evt.data + "</p>");
                         };
-
-
                     }
-                }
-            )
-
+                })
+                .fail(function () {
+                    isWorking = false;
+                    hideProgress();
+                    $("#bRankix").addClass("btn-primary").removeClass("btn-disabled");
+                    $("div#results").prepend("<p class='text-danger'>Network error occurred, Please check your connection</p>");
+                })
 
         }
 
